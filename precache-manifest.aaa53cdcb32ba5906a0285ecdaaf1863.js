2d243fbffcec64a2216451706f38d4dbfd669467
Debaprasad-dez <debaprasadpaul033@gmail.com>
2020-08-27 17:37:19 +0530

    Updates

---
+++ precache-manifest.aaa53cdcb32ba5906a0285ecdaaf1863.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5796fa2b027d275e36bf5904776e525",
+    "url": "/newsoutloud/index.html"
+  },
+  {
+    "revision": "712a127b397df5909601",
+    "url": "/newsoutloud/static/css/main.a7241672.chunk.css"
+  },
+  {
+    "revision": "1ca690b34d4c6a818c97",
+    "url": "/newsoutloud/static/js/2.a1f95d6f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/newsoutloud/static/js/2.a1f95d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712a127b397df5909601",
+    "url": "/newsoutloud/static/js/main.63bdee46.chunk.js"
+  },
+  {
+    "revision": "8ccc70060fd1289feb04",
+    "url": "/newsoutloud/static/js/runtime-main.a75b8d93.js"
+  },
+  {
+    "revision": "b70acb50a97c4b6457d0b844185d1511",
+    "url": "/newsoutloud/static/media/logo.b70acb50.png"
+  }
+]);
